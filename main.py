13a9bf8a75833d1a5fa358d59c11c6af9aea001f
kafiekun <82383246+kafiekun@users.noreply.github.com>
2023-11-03 15:17:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -135,16 +135,24 @@ def visualize_results(image, output_dict, category_index):
         image (ndarray): Visualization of the results form above.
         
     """
-    vis_util.visualize_boxes_and_labels_on_image_array(
-            image,
-            output_dict['detection_boxes'],
-            output_dict['detection_classes'],
-            output_dict['detection_scores'],
-            category_index,
-            instance_masks=output_dict.get('detection_masks'),
-            use_normalized_coordinates=True,
-            line_thickness=4)
-    
+    try:
+        with detection_graph.as_default():
+            with tf.compat.v1.Session(graph=detection_graph) as sess:
+                while not video_thread.stopped():
+                    frame = video_thread.read()
+                    if frame is None:
+                        print("Frame stream interrupted")
+                        break
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    output = run_inference_for_single_image(frame, sess, 
+                        detection_graph)
+                    output = discriminate_class(output, 
+                        classes_to_detect, category_index)
+                    processed_image = visualize_results(frame, output, 
+                        category_index)
+                    img_placeholder.image(processed_image)
+    except KeyboardInterrupt:   
+        pass
     return image
 
 
@@ -191,7 +199,8 @@ def main():
     ##video_thread = video_utils.WebcamVideoStream(video_source)
     ##video_thread.start()
     result_queue: "queue.Queue[List[Detection]]" = Queue()
-    
+        
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     try:
         with detection_graph.as_default():
             with tf.compat.v1.Session(graph=detection_graph) as sess:
@@ -211,48 +220,6 @@ def main():
     except KeyboardInterrupt:   
         pass
         
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
-    
-    # Run inference
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net.setInput(blob)
-    output = net.forward()
-    
-    h, w = image.shape[:2]
-    
-    # Convert the output array into a structured form.
-    output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
-    output = output[output[:, 2] >= score_threshold]
-    detections = [
-        Detection(
-            class_id=int(detection[1]),
-            label=CLASSES[int(detection[1])],
-            score=float(detection[2]),
-            box=(detection[3:7] * np.array([w, h, w, h])),
-        )
-        for detection in output
-    ]
-    
-    # Render bounding boxes and captions
-    for detection in detections:
-        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-        color = COLORS[detection.class_id]
-        xmin, ymin, xmax, ymax = detection.box.astype("int")
-    
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-        cv2.putText(
-            image,
-            caption,
-            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            color,
-            2,
-        )
-    
     result_queue.put(detections)
     
     return av.VideoFrame.from_ndarray(image, format="bgr24")
