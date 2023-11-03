5167884b60596d4fee322ad712c796453fa1c7fd
kafiekun <82383246+kafiekun@users.noreply.github.com>
2023-11-03 15:13:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -209,51 +209,51 @@ def main():
                         category_index)
                     img_placeholder.image(processed_image)
                     
-    def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-        image = frame.to_ndarray(format="bgr24")
-        
-        # Run inference
-        blob = cv2.dnn.blobFromImage(
-            cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    
+    # Run inference
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net.setInput(blob)
+    output = net.forward()
+    
+    h, w = image.shape[:2]
+    
+    # Convert the output array into a structured form.
+    output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
+    output = output[output[:, 2] >= score_threshold]
+    detections = [
+        Detection(
+            class_id=int(detection[1]),
+            label=CLASSES[int(detection[1])],
+            score=float(detection[2]),
+            box=(detection[3:7] * np.array([w, h, w, h])),
         )
-        net.setInput(blob)
-        output = net.forward()
-        
-        h, w = image.shape[:2]
-        
-        # Convert the output array into a structured form.
-        output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
-        output = output[output[:, 2] >= score_threshold]
-        detections = [
-            Detection(
-                class_id=int(detection[1]),
-                label=CLASSES[int(detection[1])],
-                score=float(detection[2]),
-                box=(detection[3:7] * np.array([w, h, w, h])),
-            )
-            for detection in output
-        ]
-        
-        # Render bounding boxes and captions
-        for detection in detections:
-            caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-            color = COLORS[detection.class_id]
-            xmin, ymin, xmax, ymax = detection.box.astype("int")
-        
-            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-            cv2.putText(
-                image,
-                caption,
-                (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                color,
-                2,
-            )
-        
-        result_queue.put(detections)
-        
-        return av.VideoFrame.from_ndarray(image, format="bgr24")
+        for detection in output
+    ]
+    
+    # Render bounding boxes and captions
+    for detection in detections:
+        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
+        color = COLORS[detection.class_id]
+        xmin, ymin, xmax, ymax = detection.box.astype("int")
+    
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+        cv2.putText(
+            image,
+            caption,
+            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            color,
+            2,
+        )
+    
+    result_queue.put(detections)
+    
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
     
 webrtc_streamer(
     key="web",
