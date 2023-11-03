ac1e9728a25e334d4e442f874dd49a400be702e5
kafiekun <82383246+kafiekun@users.noreply.github.com>
2023-11-03 14:42:44 +0700

    Update video_utils.py

--- video_utils.py
+++ video_utils.py
@@ -1,82 +1,7 @@
-import streamlit as st
 import cv2
 from streamlit_webrtc import webrtc_streamer
 import time
 import threading
-
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
-
-    # Run inference
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net.setInput(blob)
-    output = net.forward()
-
-    h, w = image.shape[:2]
-
-    # Convert the output array into a structured form.
-    output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
-    output = output[output[:, 2] >= score_threshold]
-    detections = [
-        Detection(
-            class_id=int(detection[1]),
-            label=CLASSES[int(detection[1])],
-            score=float(detection[2]),
-            box=(detection[3:7] * np.array([w, h, w, h])),
-        )
-        for detection in output
-    ]
-
-    # Render bounding boxes and captions
-    for detection in detections:
-        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-        color = COLORS[detection.class_id]
-        xmin, ymin, xmax, ymax = detection.box.astype("int")
-
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-        cv2.putText(
-            image,
-            caption,
-            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            color,
-            2,
-        )
-
-    result_queue.put(detections)
-
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
-
-
-webrtc_ctx = webrtc_streamer(
-    key="web",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceServers": get_ice_servers(),
-        "iceTransportPolicy": "relay",
-    },
-    video_frame_callback=video_frame_callback,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
-)
-
-if st.checkbox("Show the detected labels", value=True):
-    if webrtc_ctx.state.playing:
-        labels_placeholder = st.empty()
-        # NOTE: The video transformation with object detection and
-        # this loop displaying the result labels are running
-        # in different threads asynchronously.
-        # Then the rendered video frames and the labels displayed here
-        # are not strictly synchronized.
-        while True:
-            result = result_queue.get()
-            labels_placeholder.table(result
-
-
-webrtc_streamer(key="web", video_frame_callback=callback)
 
 class VideoStream:
     # Opens a video with OpenCV from file in a thread
@@ -101,19 +26,15 @@ class VideoStream:
             _stop_event (threading.Event): Event used to gently stop the thread.
 
         """
+        
         self.name = name
-        self.stream = webrtc_streamer(key="web")
+        self.stream = cv2.VideoCapture(src)
         self.real_time = real_time
         self.frame_rate = self.stream.get(cv2.CAP_PROP_FPS)
         self.grabbed, self.frame = self.stream.read()
         self.lock = threading.Lock()
         self._stop_event = threading.Event()
-    
-    def read(self):
-        if self.stopped():
-            print("Video ended")
-        return self.frame
-        
+
     def start(self):
         # Start the thread to read frames from the video stream with target function update
         threading.Thread(target=self.update, daemon=True, name=self.name).start()
@@ -132,6 +53,11 @@ class VideoStream:
             else:
                 return
         self.stop()
+    
+    def read(self):
+        if self.stopped():
+            print("Video ended")
+        return self.frame
 
     def stop(self):
         self.lock.acquire()
@@ -164,7 +90,7 @@ class WebcamVideoStream:
 
         """
         self.name = name
-        self.stream = webrtc_streamer(key="web")
+        self.stream = cv2.VideoCapture(src)
         self.shape = shape
         if self.shape is not None:
             self.stream.set(3, shape[0])
@@ -172,10 +98,7 @@ class WebcamVideoStream:
         self.grabbed, self.frame = self.stream.read()
         self.lock = threading.Lock()
         self._stop_event = threading.Event()
-    
-    def read(self):
-        return self.frame
-        
+
     def start(self):
         # Start the thread to read frames from the video stream
         threading.Thread(target=self.update, daemon=True, name=self.name).start()
@@ -189,6 +112,9 @@ class WebcamVideoStream:
             else:
                 return
         self.stopped
+    
+    def read(self):
+        return self.frame
 
     def stop(self):
         self.lock.acquire()
