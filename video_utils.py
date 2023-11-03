840004d987190d9881d5db92f2f50a6452cc8490
kafiekun <82383246+kafiekun@users.noreply.github.com>
2023-11-03 10:28:54 +0700

    Update video_utils.py

--- video_utils.py
+++ video_utils.py
@@ -70,7 +70,7 @@ class VideoStream:
 class WebcamVideoStream:
     # Opens a video stream with OpenCV from a wired webcam in a thread
     def __init__(self, src, shape=None, name="WebcamVideoStream"):
-        webrtc_streamer(key="sample")(src)
+        webrtc_streamer(key="sample")
         """Initialize the video stream from a video
 
         Args:
