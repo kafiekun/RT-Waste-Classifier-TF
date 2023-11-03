5ba391ece558c08afacb451e326900d49895b924
kafiekun <82383246+kafiekun@users.noreply.github.com>
2023-11-03 11:29:31 +0700

    Update video_utils.py

--- video_utils.py
+++ video_utils.py
@@ -3,6 +3,20 @@ from streamlit_webrtc import webrtc_streamer
 import time
 import threading
 
+threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
+threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+
+
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(key="Web", video_frame_callback=callback)
+
 class VideoStream:
     # Opens a video with OpenCV from file in a thread
     def __init__(self, src, name="VideoStream", real_time=True):
